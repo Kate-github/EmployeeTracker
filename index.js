bc867feeaf0f4e8ca6fb573a9058854c87a5c45d
Kate Lam <nhatthao_lam@outlook.com>
2022-08-04 14:15:42 -0500

    updated code to use promises and async/await

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { welcomeMessage } = require('./constants');
 const inquirer = require('inquirer');
 const fs = require('fs');
 const mysql = require('mysql2');
@@ -12,212 +11,258 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the emptracker_db database.`)
 );
-
+const welcomeMessage = fs.readFileSync('./welcome-msg.txt', { encoding: 'ascii' });
 function main() {
-    // console.log(welcomeMessage);
+    console.log(welcomeMessage);
     showMainMenu()
 }
 
+/**
+ * displays main menu choices
+ */
 function showMainMenu() {
     const mainMenu = JSON.parse(fs.readFileSync('./questions/main-menu.json'));
-    inquirer.prompt(mainMenu).then(answers => processMainMenu(answers));
+    inquirer.prompt(mainMenu).then(answers => processMainMenu$(answers));
 }
 
-function processMainMenu(answers) {
+/**
+ * a function that decides what actions to take when the user makes a selection
+ */
+async function processMainMenu$(answers) {
     switch (answers.whatToDo) {
         case "show-depts":
-            showDepts();
+            await showResultsInTable$('SELECT * FROM departments');
+            showMainMenu();
             break;
         case "show-roles":
-            showRoles();
+            await showResultsInTable$('SELECT * FROM roles');
+            showMainMenu();
             break;
         case "show-emps":
-            showEmps();
+            await showResultsInTable$('SELECT * FROM employees');
+            showMainMenu();
+            break;
+        case "show-all":
+            await showAll$();
+            showMainMenu();
             break;
         case "add-role":
-            addRole();
+            await addRole$();
+            showMainMenu();
             break;
         case "add-dept":
-            addDepartment();
+            await addDepartment$();
+            showMainMenu();
             break;
         case "add-emp":
-            addEmployee();
+            await addEmployee$();
+            showMainMenu();
             break;
         case "update-emp-role":
-            updateEmployeeRole();
+            await updateEmployeeRole$();
+            showMainMenu();
             break;
         default:
             process.exit();
-            return;
     }
 }
-
-function showDepts() {
-    db.query('SELECT * FROM departments', function (err, results) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.table(results);
-        showMainMenu();
-    });
+/**
+ * Displays the results of a SQL statement in a formatted table
+ * @param {string} query 
+ */
+async function showResultsInTable$(query) {
+    const results = await queryDatabase$(query);
+    console.table(results);
 }
 
-function showRoles() {
-    db.query('SELECT * FROM roles', function (err, results) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.table(results);
-        showMainMenu();
+/**
+ * Executes a SQL query and returns the result as a Promise
+ * @param {string} query 
+ * @returns Promise
+ */
+async function queryDatabase$(query) {
+    return new Promise((resolve, reject) => {
+        db.query(query, function (err, results) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
     });
 }
-
-function showEmps() {
-    db.query('SELECT * FROM employees', function (err, results) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.table(results);
-        showMainMenu();
-    });
+/**
+ * Displays all records in all tables in the database;
+ */
+async function showAll$() {
+    await showResultsInTable$('SELECT * from departments');
+    await showResultsInTable$('SELECT * from roles');
+    await showResultsInTable$('SELECT * from employees');
 }
 
-function addDepartment() {
+/**
+ * 
+ * @returns Adds a new department into Departments table
+ */
+function addDepartment$() {
     const deptQuestions = JSON.parse(fs.readFileSync('./questions/department.json'));
-    inquirer.prompt(deptQuestions).then(answer => createDepartment(answer))
+    return inquirer.prompt(deptQuestions).then(answer => createDepartment$(answer))
 }
 
-function addRole() {
+/**
+ * 
+ * @returns Adds new role in roles table
+ */
+async function addRole$() {
 
     const roleQuestions = JSON.parse(fs.readFileSync('./questions/role.json'));
-    db.query('SELECT id, department_name FROM departments', (err, results) => {
-        results.forEach((dept) => {
-            roleQuestions[2].choices.push({
-                name: dept.department_name,
-                value: dept.id
-            });
+    // Select all departments
+    const results = await queryDatabase$('SELECT id, department_name FROM departments');
+
+    // append department to choices array
+    results.forEach((dept) => {
+        roleQuestions[2].choices.push({
+            name: dept.department_name,
+            value: dept.id
         });
-        inquirer.prompt(roleQuestions).then(answers => createRole(answers));
+    });
+
+    // prompt the user to enter details for new role
+    return inquirer.prompt(roleQuestions).then(answers => createRole$(answers));
+}
+
+function createDepartment$(dept) {
+    return new Promise((resolve, reject) => {
+
+        db.query(
+            `INSERT INTO departments (department_name) VALUES (?)`,
+            [dept.department],
+            (err, result) => {
+                const msg = (err) ? 'There was an error, department not added to database.' :
+                    'Departmnet successfully added to database.';
+                console.log(msg);
+                if (err) {
+                    reject(msg);
+                } else {
+                    resolve(msg);
+                }
+            }
+        )
+    });
+};
+
+function createRole$(role) {
+    return new Promise((resolve, reject) => {
+        // Inserts role into Roles table
+        db.query(
+            `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`,
+            [role.title, role.salary, role.department_id],
+            (err, results) => {
+                const msg = (err) ? 'There was an error, role not added to database.' :
+                    'Role successfully added to database.'
+                console.log(msg);
+                if (err) {
+                    reject(msg);
+                } else {
+                    resolve(msg);
+                }
+            });
     });
 }
 
-function createDepartment(dept) {
-    db.query(
-        `INSERT INTO departments (department_name) VALUES (?)`,
-        [dept.department],
-        (err, result) => {
-            const msg = (err) ? 'There was an error, department not added to database.' :
-                'Departmnet successfully added to database.'
-            console.log(msg);
-            showMainMenu();
-        }
-    )
-};
-
-function createRole(role) {
-    db.query(
-        `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`,
-        [role.title, role.salary, role.department_id],
-        (err, results) => {
-            const msg = (err) ? 'There was an error, role not added to database.' :
-                'Role successfully added to database.'
-            console.log(msg);
-            showMainMenu();
-        });
-}
-
-function addEmployee() {
+async function addEmployee$() {
     const empQuestions = JSON.parse(fs.readFileSync('./questions/employee.json'));
-    db.query(
-        'SELECT id, title FROM roles',
-        (err, results) => {
-            results.forEach((role) => {
-                empQuestions[2].choices.push({
-                    name: role.title,
-                    value: role.id
-                });
-            });
-            db.query(
-                'SELECT id, firstName, lastName FROM employees',
-                (err, results) => {
-                    results.forEach((emp) => {
-                        empQuestions[3].choices.push({
-                            name: `${emp.firstName} ${emp.lastName}`,
-                            value: emp.id
-                        });
+    // select roles from database
+    const roles = await queryDatabase$('SELECT id, title FROM roles');
 
-                    });
-                    inquirer.prompt(empQuestions).then(answers => createEmployee(answers));
-                }
-            )
+    // append to choices
+    roles.forEach((role) => {
+        empQuestions[2].choices.push({
+            name: role.title,
+            value: role.id
         });
+    });
+
+    // select all employess
+    const managers = await queryDatabase$('SELECT id, firstName, lastName FROM employees');
+
+    // append employees to manager choices
+    managers.forEach((emp) => {
+        empQuestions[3].choices.push({
+            name: `${emp.firstName} ${emp.lastName}`,
+            value: emp.id
+        });
+    });
+
+    // prompt user to enter new employee details 
+    return inquirer.prompt(empQuestions).then(answers => createEmployee$(answers));
 }
 
-function createEmployee(emp) {
-    db.query(
-        `INSERT INTO employees (firstName, lastName, role_id, manager_id) VALUES (?,?,?,?)`,
-        [emp.firstName, emp.lastName, emp.role_id, emp.manager_id],
-        (err, result) => {
-            const msg = (err) ? 'There was an error, employee not added to database.' :
-                'Employee successfully added to database.'
-            console.log(msg);
-            showMainMenu();
-        }
-    )
-}
+function createEmployee$(emp) {
 
-function updateEmployeeRole() {
-    const selectEmpQuestion = JSON.parse(fs.readFileSync('./questions/select-employee.json'));
-    db.query(
-        'SELECT firstName, lastName, id, role_id FROM Employees',
-        (err, results) => {
-            if (err) {
-                console.log('An error occured while retrieving the list of employees');
-                return;
+    return new Promise((resolve, reject) => {
+        // inserts new record in employees table
+        db.query(
+            `INSERT INTO employees (firstName, lastName, role_id, manager_id) VALUES (?,?,?,?)`,
+            [emp.firstName, emp.lastName, emp.role_id, emp.manager_id],
+            (err, result) => {
+                const msg = (err) ? 'There was an error, employee not added to database.' :
+                    'Employee successfully added to database.';
+                console.log(msg);
+                if (err) {
+                    reject(msg);
+                } else {
+                    resolve(msg);
+                }
             }
-            results.forEach((emp) => {
-                selectEmpQuestion.choices.push({
-                    name: `${emp.firstName} ${emp.lastName}`,
-                    value: emp.id
-                })
-            });
-            inquirer.prompt(selectEmpQuestion).then((empAnswer) => {
-                const selectedEmployee = results.filter((emp) => {
-                    return emp.id === empAnswer.id
-                });
-                console.log('selected:', selectedEmployee);
-                db.query(
-                    `SELECT id, title FROM roles WHERE id <> ${selectedEmployee[0].role_id}`,
-                    (err, roleResults) => {
-                        const selectRoleQuestion = JSON.parse(fs.readFileSync('./questions/select-role.json'));
-                        roleResults.forEach(role => {
-                            selectRoleQuestion.choices.push({
-                                name: role.title,
-                                value: role.id
-                            })
-                        });
-                        inquirer.prompt(selectRoleQuestion).then((answer) => {
-                            db.query(
-                                `UPDATE employees
-                            SET role_id = ${answer.role_id}
-                            WHERE id = ${selectedEmployee[0].id};`,
-                                (err, result) => {
-                                    if (result) {
-                                        console.log(`Succesfully updated ${selectedEmployee[0].firstName}'s role`);
-                                    } else {
-                                        console.log('There was an error updating the employee\'s role.');
-                                    }
-                                    showMainMenu();
-                                }
-                            )
-                        });
-                    }
-                )
-            });
-        }
-    )
+        )
+    });
+}
+
+async function updateEmployeeRole$() {
+
+    const selectEmpQuestion = JSON.parse(fs.readFileSync('./questions/select-employee.json'));
+
+    // select all employees 
+    const employees = await queryDatabase$('SELECT firstName, lastName, id, role_id FROM Employees');
+
+    // then append the employess to the choices
+    employees.forEach((emp) => {
+        selectEmpQuestion.choices.push({
+            name: `${emp.firstName} ${emp.lastName}`,
+            value: emp.id
+        });
+    });
+
+    // prompt user to select employee to update
+    const empAnswer = await inquirer.prompt(selectEmpQuestion);
+
+    // find the selected employee from the list of employees
+    const selectedEmployee = employees.filter((emp) => {
+        return emp.id === empAnswer.id
+    });
+
+
+    // select all roles that do not match the select employee's role
+    const roleResults = await queryDatabase$(`SELECT id, title FROM roles WHERE id <> ${selectedEmployee[0].role_id}`);
+
+    // append to choices
+    const selectRoleQuestion = JSON.parse(fs.readFileSync('./questions/select-role.json'));
+    roleResults.forEach(role => {
+        selectRoleQuestion.choices.push({
+            name: role.title,
+            value: role.id
+        })
+    });
+
+    // prompt user to select new role
+    const answer = await inquirer.prompt(selectRoleQuestion)
+
+    // update user in database
+    const result = await queryDatabase$(`UPDATE employees
+    SET role_id = ${answer.role_id}
+    WHERE id = ${selectedEmployee[0].id};`);
+    
+    console.log(`Succesfully updated ${selectedEmployee[0].firstName}'s role`);
 }
 main();
